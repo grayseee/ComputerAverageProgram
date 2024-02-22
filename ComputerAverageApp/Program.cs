ec87e17140ba6f27b5f55b17185ac06242aa0c97
bayonito.290570 <bayonito.290570@LAB-A-PC14>
2024-02-23 07:55:26 +0800

    Add project files.

---
+++ ComputerAverageApp/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ComputerAverageApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ENTER 5 GRADES SEPARATED BY NEW LINE ");
+            double sum = 0;
+            for (int i = 0; i < 5; i++)
+
+            {
+               
+                double grade = Convert.ToDouble(Console.ReadLine());
+                sum += grade;
+            }
+            double average = sum / 5;
+
+            double roundedAverage = Math.Round(average);
+            Console.WriteLine("The average is: " + average + " and round off to " + roundedAverage);
+        }
+    }
+
+}
